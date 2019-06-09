1a392e8fd1568e4df76233426a6dae74ce8eb134
rvpandey <rishavpandeyjec@gmail.com>
2019-06-09 12:33:01 +0530

    downloaded from spring initializer

---
+++ src/main/java/com/gfg/rishavpandeyjec/Springbootwebapplication/SpringbootwebapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.rishavpandeyjec.Springbootwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebapplicationApplication.class, args);
+	}
+
+}
